9d1546cfea63e96ebb79e06085fb8e6378441a96
sangpotado <sangdo1995@gmail.com>
2023-10-24 12:37:38 -0700

    done with arrayexamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void SDtestReverseInPlace() {
+    int[] input1 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2,1}, input1);
+	}
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void SDtestReversed() {
+    int[] input1 = {4,3,2,1};
+    assertArrayEquals(new int[]{1,2,3,4}, ArrayExamples.reversed(input1));
   }
 }
